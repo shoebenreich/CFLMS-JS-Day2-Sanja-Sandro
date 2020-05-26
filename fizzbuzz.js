1a121673b239c21b7e8e6341862de1f596f71edc
Sanja Höbenreich <sanja.hoebenreich@outlook.at>
2020-05-26 12:50:19 +0200

    intermediate exercise part 2 and 3 added

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,10 @@
 for (var i = 1; i < 101; i++) {
-	if (i % 3 === 0) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		document.write('FizzBuzz<br>');
+	} else if (i % 3 === 0) {
 		document.write('Fizz<br>');
 	} else if (i % 5 === 0) {
 		document.write('Buzz<br>');
-	} else if (i % 3 === 0 && i % 5 === 0) {
-		document.write('Fizzbuzz<br>');
 	} else {
 		document.write(i + '<br>');
 	}
---
+++ triangle.js
@@ -0,0 +1,6 @@
+for (let u = 0; u <= 5; u++) {
+	for (let v = 0; v <= u; v++) {
+		document.write('* ');
+	}
+	document.write('<br>');
+}
